c17a9565d6d30b6b0d355474c320c8d4277a2c3a
amir hossein <amirhossein00978@gmail.com>
2025-01-08 02:51:17 +0330

    addproject

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Models;
+using TaskManagerAPI.Data;
+
+namespace TaskManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTasks()
+        {
+            var tasks = _context.Tasks.ToList();
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask([FromBody] Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetAllTasks), new { id = task.Id }, task);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using TaskManagerAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
